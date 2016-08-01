95b144fdfad31b3b4a72a64f63aeb812b5cbfd93
mokena <443912790@qq.com>
2016-08-01 21:05:01 +0800

    add bird to physics world

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,52 @@
+#include "HelloWorldScene.h"
+#include "SimpleAudioEngine.h"
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::createWithPhysics();
+    
+	//set world's gravity
+	scene->getPhysicsWorld()->setGravity(Vec2(0, -10));
+
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	bird = Sprite::create("bird.png");
+	auto birdBody = PhysicsBody::createBox(bird->getContentSize());
+	bird->setPhysicsBody(birdBody);
+	bird->setPosition(visibleSize / 4);
+	addChild(bird);
+    
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
